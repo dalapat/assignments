502935f5b67a448335d6150a69665540d4c0cfb5
Anish <anishdalal95@gmail.com>
2016-02-17 12:12:16 -0500

    comment

--- compilers2/Scanner.py
+++ compilers2/Scanner.py
@@ -10,9 +10,28 @@ class Scanner:
         i = 0
         input_text_length = len(self.input_text)
         tokenlist = []
+        error_flag = 0
         while(i < input_text_length):
             if self.is_whitespace(self.input_text[i]):
                 i += 1
+            elif not i+1 >= input_text_length and self.is_open_comment(self.input_text[i], i):
+                open_comment_count = 1
+                closed_comment_count = 0
+                i += 2
+                while(i < input_text_length and not open_comment_count == closed_comment_count):
+                    if self.is_open_comment(self.input_text[i], i):
+                        open_comment_count += 1
+                        i += 2
+                    elif self.is_closed_comment(self.input_text[i], i):
+                        closed_comment_count += 1
+                        i += 2
+                    else:
+                        i += 1
+                if not open_comment_count == closed_comment_count:
+                    print "error: unclosed comment"
+                    error_flag = 1
+                    break
+
             elif self.is_letter(self.input_text[i]):
                 v = []
                 start = i
@@ -20,7 +39,7 @@ class Scanner:
                                                  or self.is_digit(self.input_text[i]))):
                     v.append(self.input_text[i])
                     i += 1
-                end = i
+                end = i-1
                 v_string = ''.join(v)
                 try:
                     token_string = Token.keyword_map[v_string]
@@ -37,7 +56,7 @@ class Scanner:
                 while(i < input_text_length and self.is_digit(self.input_text[i])):
                     v = (10*v) + int(self.input_text[i])
                     i += 1
-                end = i
+                end = i-1
                 token = Token(kind=0, int_value=v, start_position=start, end_position=end)
                 tokenlist.append(token)
             elif self.is_symbol(self.input_text[i]):
@@ -52,12 +71,22 @@ class Scanner:
                     tokenlist.append(token)
                     i+=1
             else:
-                print "error"
-        tokenlist.append(Token(kind=4, eof_value="eof", start_position=i, end_position=i))
+                print "error: illegal character \'{0}\' found " \
+                      "at position @({1}, {2})".format(self.input_text[i], i, i)
+                error_flag = 1
+                break
+        if error_flag == 0:
+            tokenlist.append(Token(kind=4, eof_value="eof", start_position=i, end_position=i))
         return tokenlist
 
     def is_whitespace(self, c):
         return c == ' ' or c == '\t' or c == '\n' or c == '\r'
+
+    def is_open_comment(self, c, i):
+        return (c+self.input_text[i+1]) == "(*"
+
+    def is_closed_comment(self, c, i):
+        return (c+self.input_text[i+1]) == "*)"
 
     def is_letter(self, c):
         # if scanner reads a letter, it could be a keyword or an identifier
@@ -85,7 +114,10 @@ class Scanner:
         pass
 
 def main():
-    s = Scanner("VAR ics142: ARRAY 5 OF INTEGER;")
+    # s = Scanner("VAR ics142: ARRAY 5 OF INTEGER;")
+    # s = Scanner("asdf 8_9 * ()^D")
+    s = Scanner("(* outside test (*inside test*)*) *)")
+    # s = Scanner("PROGRAM As3;\nCONST x = -47;TYPE T = RECO$RD f: INTEGER; END; VAR a: 123ARRAY123 12 OF T; BEGINa[7].f := -x/END As3.")
     for elem in s.tokenlist:
         print elem
 
