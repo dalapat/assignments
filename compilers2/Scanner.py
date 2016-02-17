52c1233f01c2602f3ea615ca5accfa25a2df3832
Anish <anishdalal95@gmail.com>
2016-02-17 00:13:00 -0500

    scanned

--- compilers2/Scanner.py
+++ compilers2/Scanner.py
@@ -4,16 +4,33 @@ class Scanner:
 
     def __init__(self, input_text):
         self.input_text = input_text
+        self.tokenlist = self.form_tokens()
 
     def form_tokens(self):
-        tokenlist = []
         i = 0
         input_text_length = len(self.input_text)
+        tokenlist = []
         while(i < input_text_length):
             if self.is_whitespace(self.input_text[i]):
                 i += 1
             elif self.is_letter(self.input_text[i]):
-                pass
+                v = []
+                start = i
+                while(i < input_text_length and (self.is_letter(self.input_text[i])
+                                                 or self.is_digit(self.input_text[i]))):
+                    v.append(self.input_text[i])
+                    i += 1
+                end = i
+                v_string = ''.join(v)
+                try:
+                    token_string = Token.keyword_map[v_string]
+                    token = Token(kind=2, keyword_value=token_string,
+                                  start_position=start, end_position=end)
+                    tokenlist.append(token)
+                except:
+                    token = Token(kind=1, identifier_value=v_string,
+                                  start_position=start, end_position=end)
+                    tokenlist.append(token)
             elif self.is_digit(self.input_text[i]):
                 v = 0
                 start = i
@@ -24,9 +41,20 @@ class Scanner:
                 token = Token(kind=0, int_value=v, start_position=start, end_position=end)
                 tokenlist.append(token)
             elif self.is_symbol(self.input_text[i]):
-                pass
+                symbol = self.input_text[i]
+                if(not i == input_text_length-1 and (self.input_text[i]+self.input_text[i+1]) in Token.symbol_map):
+                    symbol = self.input_text[i] + self.input_text[i+1]
+                    token = Token(kind=3, symbol_value=symbol, start_position=i, end_position=i+1)
+                    tokenlist.append(token)
+                    i+=2
+                else:
+                    token = Token(kind=3, symbol_value=symbol, start_position=i, end_position=i)
+                    tokenlist.append(token)
+                    i+=1
             else:
                 print "error"
+        tokenlist.append(Token(kind=4, eof_value="eof", start_position=i, end_position=i))
+        return tokenlist
 
     def is_whitespace(self, c):
         return c == ' ' or c == '\t' or c == '\n' or c == '\r'
@@ -56,3 +84,9 @@ class Scanner:
     def all(self):
         pass
 
+def main():
+    s = Scanner("VAR ics142: ARRAY 5 OF INTEGER;")
+    for elem in s.tokenlist:
+        print elem
+
+main()
--- compilers2/Token.py
+++ compilers2/Token.py
@@ -1,21 +1,22 @@
 class Token():
 
     keyword_list = ["CONST", "PROGRAM", "BEGIN", "END", "TYPE", "VAR",
-                    "ARRAY", "IF", "DIV", "ELSE", "REPEAT", "WHILE", "WRITE", "READ",
-                    "THEN", "END", "MOD", "DO", "UNTIL"]
+                    "ARRAY", "IF", "OF", "DIV", "ELSE", "REPEAT", "WHILE", "WRITE", "READ",
+                    "THEN", "END", "MOD", "DO", "RECORD", "UNTIL"]
     symbol_list = ["+", "-", "*", ":", ";", "=", "<", ">", "<=",
                    ">=", "#", ":=", "(", ")", "[", "]", ",", "."]
     # peek symbols, : -> := | < -> <= | > -> >= | ( -> (*
     keyword_map = {i:i for i in keyword_list}
-    symbol_list = {k:k for k in symbol_list}
+    symbol_map = {k:k for k in symbol_list}
 
     def __init__(self, kind=0, int_value=0, identifier_value="", keyword_value="",
-                 symbol="", start_position=0, end_position=0):
+                 symbol_value="", eof_value = "eof", start_position=0, end_position=0):
         self.kind = kind #integer=0, identifier=1, keyword=2, symbol=3, eof=4
         self.int_value = int_value
         self.identifier_value = identifier_value
         self.keyword_value = keyword_value
-        self.symbol = symbol
+        self.symbol_value = symbol_value
+        self.eof_value = eof_value
         self.start_position = start_position
         self.end_position = end_position
 
@@ -32,10 +33,10 @@ class Token():
             output_string = "{0}@({1}, {2})".format(self.keyword_value, self.start_position, self.end_position)
         elif self.kind == 3:
             # symbol
-            output_string = "{0}@({1}, {2})".format(self.symbol, self.start_position, self.end_position)
+            output_string = "{0}@({1}, {2})".format(self.symbol_value, self.start_position, self.end_position)
         elif self.kind == 4:
             # eof
-            output_stirng = "eof@{0}, {1}".format(self.start_position, self.end_position)
+            output_string = "{0}@({1}, {1})".format(self.eof_value, self.start_position, self.end_position)
         else:
             output_string = "error: not a valid token"
-        print output_string
+        return output_string
