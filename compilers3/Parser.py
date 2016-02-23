12f27dbed5e4458870ba9b96201659e926ac452e
Anish <anishdalal95@gmail.com>
2016-02-22 22:31:53 -0500

    parser

--- compilers3/Parser.py
+++ compilers3/Parser.py
@@ -100,43 +100,132 @@ class Parser:
 
     def _expression(self):
         print "Expression"
-        
+        if self.token_list[self.current].kind == self.kind_map["+"]:
+            self.match("+")
+        elif self.token_list[self.current].kind == self.kind_map["-"]:
+            self.match("-")
+        self._term()
+        while (self.token_list[self.current].kind == self.kind_map["+"]) or \
+                (self.token_list[self.current].kind == self.kind_map["-"]):
+            if self.token_list[self.current].kind == self.kind_map["+"]:
+                self.match("+")
+            elif self.token_list[self.current].kind == self.kind_map["-"]:
+                self.match("-")
+            else:
+                sys.stderr.write("error: expected {0}, received unexpected "
+                                 "token {1}".format("+ or -", self.token_list[self.current]))
+            self._term()
 
     def _term(self):
-        pass
+        print "Term"
+        self._factor()
+        while (self.token_list[self.current].kind == self.kind_map["*"])or \
+            (self.token_list[self.current].kind == self.kind_map["DIV"]) or \
+            (self.token_list[self.current].kind == self.kind_map["MOD"]):
+            self._factor()
 
     def _factor(self):
-        pass
+        print "Factor"
+        if self.token_list[self.current].kind == self.kind_map["INTEGER"]:
+            self.match("INTEGER")
+        elif self.token_list[self.current].kind == self.kind_map["IDENTIFIER"]:
+            self._designator()
+        elif self.token_list[self.current].kind == self.kind_map["("]:
+            self.match("(")
+            self._expression()
+            self.match(")")
+        else:
+            sys.stdout.error("error: idk man something's wrong in factor")
 
     def _instructions(self):
-        pass
+        print "Instructions"
+        self._instruction()
+        while self.token_list[self.current].kind == self.kind_map[";"]:
+            self.match(";")
+            self._instruction()
 
     def _instruction(self):
-        pass
+        print "Instruction"
+        # is this bad that i check directly for identifier?
+        if self.token_list[self.current].kind == self.kind_map["IDENTIFIER"]:
+            self._assign()
+        elif self.token_list[self.current].kind == self.kind_map["IF"]:
+            self._if()
+        elif self.token_list[self.current].kind == self.kind_map["REPEAT"]:
+            self._repeat()
+        elif self.token_list[self.current].kind == self.kind_map["WHILE"]:
+            self._while()
+        elif self.token_list[self.current].kind == self.kind_map["READ"]:
+            self._read()
+        elif self.token_list[self.current].kind == self.kind_map["WRITE"]:
+            self._write()
+        else:
+            sys.stderr.write("error: instruction method")
 
     def _assign(self):
-        pass
+        print "Assign"
+        self._designator()
+        self.match(":=")
+        self._expression()
 
     def _if(self):
-        pass
+        print "If"
+        self.match("IF")
+        self._condition()
+        self.match("THEN")
+        self._instructions()
+        if self.token_list[self.current].kind == self.kind_map["ELSE"]:
+            self.match("ELSE")
+            self._instructions()
+        self.match("END")
 
     def _repeat(self):
-        pass
+        print "Repeat"
+        self.match("REPEAT")
+        self._instructions()
+        self.match("UNTIL")
+        self._condition()
+        self.match("END")
 
     def _while(self):
-        pass
+        print "While"
+        self.match("WHILE")
+        self._condition()
+        self.match("DO")
+        self._instructions()
+        self.match("END")
 
     def _condition(self):
-        pass
+        print "Condition"
+        self._expression()
+        ("="|"#"|"<"|">"|"<="|">=")
+        if self.token_list[self.current].kind == self.kind_map["="]:
+            self.match("=")
+        elif self.token_list[self.current].kind == self.kind_map["#"]:
+            self.match("#")
+        elif self.token_list[self.current].kind == self.kind_map["<"]:
+            self.match("<")
+        elif self.token_list[self.current].kind == self.kind_map[">"]:
+            self.match(">")
+        elif self.token_list[self.current].kind == self.kind_map["<="]:
+            self.match("<=")
+        elif self.token_list[self.current].kind == self.kind_map[">="]:
+            self.match(">=")
+        else:
+            sys.stderr.write("error: condition method")
 
     def _write(self):
-        pass
+        print "Write"
+        self.match("WRITE")
+        self._expression()
 
     def _read(self):
-        pass
+        print "Read"
+        self.match("READ")
+        self._designator()
 
     def _designator(self):
-        pass
+        
 
     def _selector(self):
         pass
