afa51aa82ee25977a91e32702a83b4cf20d57a1e
Anish <anishdalal95@gmail.com>
2016-02-22 21:32:07 -0500

    parser 1

---
+++ compilers3/Parser.py
@@ -0,0 +1,141 @@
+from UnexpectedTokenException import UnexpectedTokenException
+from Scanner import Scanner
+from Token import Token
+
+import sys
+
+class Parser:
+
+    def __init__(self, token_list):
+        self.current = 0
+        self.token_list = token_list
+        # fix this
+        t = Token()
+        self.kind_map = t.kind_map
+
+    def parse(self):
+        self._program()
+
+    def match(self, kind):
+        if self.token_list[self.current].kind == self.kind_map[kind]:
+            sys.stdout.write(self.token_list[self.current])
+            self.current += 1
+        else:
+            sys.stderr.write("error: expected token kind {0}, "
+                             "received unexpected token {1}".format(kind, self.token_list[self.current]))
+
+    def _program(self):
+        print "Program"
+        self.match("PROGRAM")
+        self.match("IDENTIFIER")
+        self.match(";")
+        self._declarations()
+        if self.token_list[self.current].kind == self.kind_map["BEGIN"]:
+            self.match("BEGIN")
+            self._instructions()
+        self.match("END")
+        self.match("IDENTIFIER")
+        self.match(".")
+
+    def _declarations(self):
+        print "Declarations"
+        while (self.token_list[self.current].kind == self.kind_map["CONST"] or
+            self.token_list[self.current].kind == self.kind_map["TYPE"] or
+            self.token_list[self.current].kind == self.kind_map["VAR"]):
+            if self.token_list[self.current].kind == self.kind_map["CONST"]:
+                self._constdecl()
+            elif self.token_list[self.current].kind == self.kind_map["TYPE"]:
+                self._typedecl()
+            elif self.token_list[self.current].kind == self.kind_map["VAR"]:
+                self._vardecl()
+            else:
+                pass
+
+    def _constdecl(self):
+        print "ConstDecl"
+        self.match("CONST")
+        while self.token_list[self.current].kind == self.kind_map["IDENTIFIER"]:
+            self.match("IDENTIFIER")
+            self.match("=")
+            self._expression()
+            self.match(";")
+
+    def _typedecl(self):
+        print "TypeDecl"
+        self.match("TYPE")
+        while self.token_list[self.current].kind == self.kind_map["IDENTIFIER"]:
+            self.match("IDENTIFIER")
+            self.match("=")
+            self._expression()
+            self.match(";")
+
+    def _vardecl(self):
+        print "VarDecl"
+        self.match("VAR")
+        
+
+    def _type(self):
+        pass
+
+    def _expression(self):
+        pass
+
+    def _term(self):
+        pass
+
+    def _factor(self):
+        pass
+
+    def _instructions(self):
+        pass
+
+    def _instruction(self):
+        pass
+
+    def _assign(self):
+        pass
+
+    def _if(self):
+        pass
+
+    def _repeat(self):
+        pass
+
+    def _while(self):
+        pass
+
+    def _condition(self):
+        pass
+
+    def _write(self):
+        pass
+
+    def _read(self):
+        pass
+
+    def _designator(self):
+        pass
+
+    def _selector(self):
+        pass
+
+    def _identifier_list(self):
+        pass
+
+    def _expression_list(self):
+        pass
+
+    def _identifier(self):
+        pass
+
+    def _integer(self):
+        pass
+
+
+def main():
+    s = Scanner("")
+
+    token_list = s.all()
+    p = Parser(token_list)
+
+main()
---
+++ compilers3/Token.py
@@ -0,0 +1,69 @@
+class Token():
+
+    """
+    This class represents a single token. A token contains a "kind" field to represent
+    whether its an integer, an identifier, a keyword, a symbol, or an EOF.
+    This class also contains a list of all valid keywords and symbols, though
+    the actual error checking is done in Scanner.
+    """
+    # list of all valid keywords
+    keyword_list = ["CONST", "PROGRAM", "BEGIN", "END", "TYPE", "VAR",
+                    "ARRAY", "IF", "OF", "DIV", "ELSE", "REPEAT", "WHILE", "WRITE", "READ",
+                    "THEN", "END", "MOD", "DO", "RECORD", "UNTIL"] #21
+
+    # list of all valid symbols
+    symbol_list = ["+", "-", "*", ":", ";", "=", "<", ">", "<=",
+                   ">=", "#", ":=", "(", ")", "[", "]", ",", "."]
+
+    # maps are formed from the above lists for constant time access
+    # to check if a token is a keyword or symbol.
+    keyword_map = {i:i for i in keyword_list}
+    symbol_map = {k:k for k in symbol_list}
+
+    kind_map = {"INTEGER":0, "IDENTIFIER":1}
+    map_count = 2
+    for keyword in keyword_list:
+        kind_map[keyword] = map_count
+        map_count += 1
+
+    for operator in symbol_list:
+        kind_map[operator] = map_count
+        map_count += 1
+
+    kind_map["EOF"] = map_count
+
+    # initialize a generic token
+    def __init__(self, kind=0, int_value=0, identifier_value="", keyword_value="",
+                 symbol_value="", eof_value = "eof", start_position=0, end_position=0):
+        self.kind = kind # INTEGER, IDENTIFIER, <keyword>, <symbol>, EOF
+        self.int_value = int_value # actual integer value if integer
+        self.identifier_value = identifier_value # actual identifier value if identifier
+        self.keyword_value = keyword_value # actual keyword value if keyword
+        self.symbol_value = symbol_value # actual symbol value if symbol
+        self.eof_value = eof_value # token returned to represent eof
+        self.start_position = start_position # position where current token was found
+        self.end_position = end_position # position where current token ends
+
+    # print custom attributes of token
+    def __str__(self):
+        output_string = ""
+        if self.kind == 0:
+            # integer
+            output_string = "integer<{0}>@({1}, {2})".format(self.int_value, self.start_position, self.end_position)
+        elif self.kind == 1:
+            # identifier
+            output_string = "identifier<{0}>@({1}, {2})".format(self.identifier_value, self.start_position, self.end_position)
+        # elif self.kind == 2:
+        elif 2 <= self.kind <= len(self.keyword_list) + 1:
+            # keyword
+            output_string = "{0}@({1}, {2})".format(self.keyword_value, self.start_position, self.end_position)
+        # elif self.kind == 3:
+        elif len(self.keyword_list) + 2 <= self.kind <= len(self.symbol_list) + len(self.keyword_list) + 1:
+            # symbol
+            output_string = "{0}@({1}, {2})".format(self.symbol_value, self.start_position, self.end_position)
+        elif self.kind == 41:
+            # eof
+            output_string = "{0}@({1}, {1})".format(self.eof_value, self.start_position, self.end_position)
+        else:
+            output_string = "error: not a valid token"
+        return output_string
